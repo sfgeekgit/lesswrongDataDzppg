f46a1c8697457c6f03261b5623851025c0bb05ef
sfgeekgit <sfgeek@gmail.com>
2024-01-18 17:10:20 -0800

    data input formating

--- convert_input.py
+++ convert_input.py
@@ -5,7 +5,7 @@ import ast
 df = pd.read_csv('ZPPG_performance_data.csv')
 
 # Convert categorical columns to one-hot encoding
-categorical_columns = ['Strange Smell?', 'Air Tastes Like', 'Feng Shui of Surrounding Area']
+categorical_columns = ['Strange Smell?', 'Air Tastes Like']
 df = pd.get_dummies(df, columns=categorical_columns)
 
 # Handle the 'Weird Sounds' column
@@ -24,8 +24,12 @@ for sound in unique_sounds:
 # Drop the original 'Weird Sounds' column
 df.drop('Weird Sounds', axis=1, inplace=True)
 
+# Convert 'Feng Shui of Surrounding Area' to numerical values
+feng_shui_mapping = {'Disharmonious': 0, 'Adequate': 0.5, 'Exceptional': 1}
+df['Feng Shui of Surrounding Area'] = df['Feng Shui of Surrounding Area'].map(feng_shui_mapping)
+
 # Convert 'ZPPG Performance' from percentage to a float
 df['ZPPG Performance'] = df['ZPPG Performance'].str.rstrip('%').astype('float') / 100.0
 
 # Save the processed data to a new CSV file
-df.to_csv('processed_ZPPG_performance_data.csv', index=False)
+df.to_csv('measured_data.csv', index=False)
